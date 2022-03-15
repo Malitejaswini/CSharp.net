2814461d7dcd69ca2883b6e645b65444417e7967
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-15 13:50:32 +0530

    Add files via upload

--- whileloop.cs
+++ whileloop.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace while1
+namespace whileloop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            while (i <= 10)
+            while(i <= 10)
             {
                 if (i % 2 == 0)
                 {
                     Console.WriteLine(i);
-                    if (i == 6)
-                    {
-                        break;
-                    }
                 }
                 i++;
             }
