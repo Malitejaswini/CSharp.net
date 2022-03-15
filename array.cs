cdebc32710b4524b6ca9087e0f3c2540a9370d58
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-15 13:29:15 +0530

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []a=new int[10];
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Elements are:-");
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+                Console.ReadLine();
+        }
+    }
+}
