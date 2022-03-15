70220a60eb5daf82eccff134cc6e959441d8e9c2
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-15 18:43:20 +0530

    Add files via upload

---
+++ singleinheritance3.cs
@@ -0,0 +1,44 @@
+﻿/* 
+ * Que:- Program to check whether given number is positive or negative using single inheritance.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nopn
+{
+       public class p1
+{
+ public int n;
+  public void read()
+  {
+    Console.WriteLine("Enter the no:-");
+    n=Convert.ToInt32(Console.ReadLine());
+  }
+}
+public class p2:p1
+{
+  public void display()
+  { 
+    if(n>0)
+    {
+      Console.WriteLine("Number is positive");
+    }
+    else
+    {
+      Console.WriteLine("Number is negative");
+    }
+  }
+}
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       p2 p=new p2();
+       p.read();
+       p.display();
+       Console.ReadLine();
+     }
+   }
+ }
+
