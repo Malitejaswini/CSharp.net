09192f58f24ccc5c559fbd47e01fb6506eeea199
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-15 13:44:30 +0530

    Add files via upload

---
+++ arithmeticop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f, g;
+            Console.WriteLine("Enter 1st no.");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd no.");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+            g = a % b;
+            Console.WriteLine("Addition : "+c+"\nSubtraction : "+d+"\nMultiplication : "+e+"\nDivision : "+f+"\nMod : "+g);
+        }
+    }
+}
